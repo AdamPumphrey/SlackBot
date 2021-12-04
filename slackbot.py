b29459f47c1ecb071225afe6da163931023022e9
Adam <a.pumphrey15@gmail.com>
2021-12-04 06:07:55 +0000

    in progress reaction event

--- slackbot.py
+++ slackbot.py
@@ -20,60 +20,70 @@ TRIAGE_ID = "C02NT3DTRF1"
 CHANNELS = [TRIAGE_ID]
 
 message_counts = {}
-welcome_messages = {}
+tickets = {}
+used_ticket_ids = []
 
 # test to see if bot connects to channel
 #client.chat_postMessage(channel='#triage-servicedesk', text='Test message')
 
-class WelcomeMessage:
-    START_TEXT = {
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': (
-                'Welcome! \n\n'
-                '*Get started by completing the tasks:*'
-            )
-        }
-    }
+class Ticket:
+    # START_TEXT = {
+    #     'type': 'section',
+    #     'text': {
+    #         'type': 'mrkdwn',
+    #         'text': (
+    #         )
+    #     }
+    # }
 
-    DIVIDER = {'type': 'divider'}
+    #DIVIDER = {'type': 'divider'}
 
-    def __init__(self, channel, user):
-        self.channel = channel
+    def __init__(self, user, user_name, text, ticket_id):
+        self.channel = CHANNELS[0]
         self.user = user
         self.timestamp = ''
-        self.completed = False
+        self.status = 'Unassigned'
+        self.user_name = user_name
+        self.ticket_id = ticket_id
+        self.text = text
 
     def get_message(self):
         return {
             'ts': self.timestamp,
             'channel': self.channel,
             'blocks': [
-                self.START_TEXT,
-                self.DIVIDER,
-                self._get_reaction_task() 
+                #self.START_TEXT,
+                #self.DIVIDER,
+                self._get_ticket() 
             ]
         }
 
-    def _get_reaction_task(self):
-        checkmark = ':white_check_mark:'
-        if not self.completed:
-            checkmark = ':white_large_square:'
+    def _get_ticket(self):
+        #checkmark = ':white_check_mark:'
+        #if not self.completed:
+        #    checkmark = ':white_large_square:'
+        
+        ticket_info=f'Ticket #{self.ticket_id}:\n\nUser: {self.user_name}\n\nDescription: {self.text}\n\nStatus: {self.status}'
+        #channel=CHANNELS[0], text=f'Ticket #{ticket_id}:\n\nUser: {user_name}\n\nDescription: {text}\n\nStatus: Unassigned'
 
-        text = f'{checkmark} *React to this message!*'
+        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': ticket_info}}
+    
+    def _get_ticket_update(self):
+        ticket_info=f'Status for Ticket #{self.ticket_id}: {self.status}'
 
-        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
+        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': ticket_info}}
 
-def send_welcome_message(channel, user):
-    welcome = WelcomeMessage(channel, user)
-    message = welcome.get_message()
+def create_ticket(user, user_name, text, ticket_id):
+    ticket = Ticket(user, user_name, text, ticket_id)
+    message = ticket.get_message()
     response = client.chat_postMessage(**message)
-    welcome.timestamp = response['ts']
+    ticket.timestamp = response['ts']
 
-    if channel not in welcome_messages:
-        welcome_messages[channel] = {}
-        welcome_messages[channel][user] = welcome
+    if user not in tickets:
+        tickets[user] = {}
+        tickets[user][ticket_id] = ticket
+    else:
+        tickets[user][ticket_id] = ticket
 
 # event on team join send welcome message
 
@@ -82,36 +92,36 @@ def message(payload):
     print(payload)
     event= payload.get('event', {})
     channel_id = event.get('channel')
-    # print(channel_id)
     user_id = event.get('user')
     text = event.get('text')
+    print(text)
 
     if user_id != None and BOT_ID != user_id:
-        if user_id in message_counts and channel_id in CHANNELS:
-            message_counts[user_id] += 1
-           #client.chat_postMessage(channel='#triage-servicedesk', text=text)
-        else:
-            message_counts[user_id] = 1
-        #client.chat_postMessage(channel='#triage-servicedesk', text=text)
-        if text.lower() == 'start':
-            send_welcome_message(f'@{user_id}', user_id)
+        # if user_id in message_counts and channel_id in CHANNELS:
+        #     message_counts[user_id] += 1
+        #    #client.chat_postMessage(channel='#triage-servicedesk', text=text)
+        # else:
+        #     message_counts[user_id] = 1
+        # #client.chat_postMessage(channel='#triage-servicedesk', text=text)
+        # if text.lower() == 'start':
+        #     send_welcome_message(f'@{user_id}', user_id)
+        return
 
 @slack_event_adapter.on('reaction_added')
 def reaction(payload):
+    print(payload)
     event= payload.get('event', {})
     channel_id = event.get('item', {}).get('channel')
     user_id = event.get('user')
 
-    # if channel_id in CHANNELS:
-    if f'@{user_id}' not in welcome_messages:
+    if channel_id in CHANNELS:
         return
-    
-    welcome = welcome_messages[f'@{user_id}'][user_id]
-    welcome.completed = True
-    welcome.channel = channel_id
-    message = welcome.get_message()
-    updated_message = client.chat_update(**message)
-    welcome.timestamp = updated_message['ts']
+        # welcome = welcome_messages[f'@{user_id}'][user_id]
+        # welcome.completed = True
+        # welcome.channel = channel_id
+        # message = welcome.get_message()
+        # updated_message = client.chat_update(**message)
+        # welcome.timestamp = updated_message['ts']
 
 
 @app.route('/ticket-count', methods=['POST'])
@@ -124,16 +134,21 @@ def ticket_count():
     return Response(), 200
 
 @app.route('/create-ticket', methods=['POST'])
-def create_ticket():
+def generate_ticket():
     data = request.form
-    print(data)
+    user_id = data.get('user_id')
     user_name = data.get('user_name')
     channel_id = data.get('channel_id')
     text = data.get('text')
     ticket_id = randrange(1000, 10000)
-    client.chat_postMessage(channel=CHANNELS[0], text=f'Ticket #{ticket_id}:\n\nUser: {user_name}\n\nDescription: {text}\n\nStatus: Unassigned')
-    client.chat_postMessage(channel=channel_id, text=f'Your ticket #{ticket_id} has been generated.\n\nDescription: {text}\n\nStatus: Unassigned')
+    while ticket_id in used_ticket_ids:
+        ticket_id = randrange(1000, 10000)
+    create_ticket(user_id, user_name, text, ticket_id)
+    #client.chat_postMessage(channel=CHANNELS[0], text=f'Ticket #{ticket_id}:\n\nUser: {user_name}\n\nDescription: {text}\n\nStatus: Unassigned')
+    client.chat_postMessage(channel=channel_id, text=f'Your ticket #{ticket_id} has been generated.\n\nDescription: {text}\n\nStatus: Unassigned\n\nCheck the status of your ticket with the /ticket-status command.')
     return Response(), 200
+
+# ticket-status command goes here
 
 if __name__ == "__main__":
     app.run(debug=True)
